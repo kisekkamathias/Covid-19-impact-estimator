6abfceabba225ceeda83b843bb2dc7f976d58498
kisekkamathias <kisekkamathias12@gmail.com>
2021-02-18 14:52:06 +0300

    making correct indentations and screating spaces between operators

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,10 @@
 const covid19ImpactEstimator = () => {
   // Data output after the computations from the estumator
-  let dataOutPut = {
+  const dataOutPut = {
     data: {},
     estmate: {
-       impact: {}, 
-       severeImpact: {}
+      impact:{}, 
+      severeImpact:{}
     }
   }; 
   // input data to the estimator
@@ -27,19 +27,19 @@ const covid19ImpactEstimator = () => {
   const period = dataOutPut.data.periodType;
   const beds = dataOutPut.data.totalHospitalBeds;
   const avgDailyIncome = dataOutPut.data.region.avgDailyIncomeInUSD;
-  const avgDailyIncomePopulation = (dataOutPut.data.region.avgDailyIncomePopulation);
-  var power;
+  const avgDailyIncomePopulation = dataOutPut.data.region.avgDailyIncomePopulation;
   // Determine period when given days, weeks or months
-  if(period === 'months'){
+  let power;
+  if (period === 'months') {
     days *= 30;
     power = Math.trunc(days / 3);
-  }else if(period === 'weeks'){
+  }else if (period === 'weeks') {
     days *= 7;
     power = Math.trunc(days / 3);
-  }else if(period === 'days'){
+  }else if (period === 'days') {
     power = Math.trunc(days / 3);
-  };
-   // Initialising variables to be accessed for computations in impact and severeImpact functions
+  }
+  // Initialising variables to be accessed for computations in impact and severeImpact functions
   let currentlyInfected;
   let infectionsByRequestedTime;
   let severeCasesByRequestedTime;
@@ -48,46 +48,40 @@ const covid19ImpactEstimator = () => {
   let casesForVentilatorsByRequestedTime;
   let dollarsInFlight;
   // computations for the impact
-  impact = () => {
-    currentlyInfected = (reported* 10);
-    infectionsByRequestedTime = Math.trunc(currentlyInfected * Math.pow(2,power));
-    severeCasesByRequestedTime = Math.trunc((15/100)*infectionsByRequestedTime);
-    hospitalBedsByRequestedTime = Math.trunc(((35/100)*beds)-severeCasesByRequestedTime);
-    casesForICUByRequestedTime = Math.trunc((5/100)*infectionsByRequestedTime);
-    casesForVentilatorsByRequestedTime = Math.trunc((2/100)*infectionsByRequestedTime);
-    dollarsInFlight =Math.trunc((infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncome) /days);
-    dataOutPut.estmate.impact = {
-      currentlyInfected,
-      infectionsByRequestedTime,
-      severeCasesByRequestedTime,
-      hospitalBedsByRequestedTime,
-      casesForICUByRequestedTime,
-      casesForVentilatorsByRequestedTime,
-      dollarsInFlight
-    };
+  currentlyInfected = (reported * 10);
+  infectionsByRequestedTime = Math.trunc(currentlyInfected * Math.pow(2,power));
+  severeCasesByRequestedTime = Math.trunc((15/100) * infectionsByRequestedTime);
+  hospitalBedsByRequestedTime = Math.trunc(((35/100) * beds)-severeCasesByRequestedTime);
+  casesForICUByRequestedTime = Math.trunc((5/100) * infectionsByRequestedTime);
+  casesForVentilatorsByRequestedTime = Math.trunc((2/100) * infectionsByRequestedTime);
+  dollarsInFlight =Math.trunc((infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncome) / days);
+  dataOutPut.estmate.impact = {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
   };
-  impact();
 
   // computations for severeImpact
-  severeImpact = () => {
-    currentlyInfected = (reported* 50);
-    infectionsByRequestedTime = Math.trunc(currentlyInfected * Math.pow(2,power));
-    severeCasesByRequestedTime = Math.trunc((15/100)*infectionsByRequestedTime);
-    hospitalBedsByRequestedTime = Math.trunc(((35/100)*beds)-severeCasesByRequestedTime);
-    casesForICUByRequestedTime = Math.trunc((5/100)*infectionsByRequestedTime);
-    casesForVentilatorsByRequestedTime = Math.trunc((2/100)*infectionsByRequestedTime);
-    dollarsInFlight =Math.trunc((infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncome) /days);
-    dataOutPut.estmate.severeImpact = {
-      currentlyInfected,
-      infectionsByRequestedTime,
-      severeCasesByRequestedTime,
-      hospitalBedsByRequestedTime,
-      casesForICUByRequestedTime,
-      casesForVentilatorsByRequestedTime,
-      dollarsInFlight
-    };
+  currentlyInfected = (reported * 50);
+  infectionsByRequestedTime = Math.trunc(currentlyInfected * Math.pow(2,power));
+  severeCasesByRequestedTime = Math.trunc((15/100) * infectionsByRequestedTime);
+  hospitalBedsByRequestedTime = Math.trunc(((35/100 ) *beds)-severeCasesByRequestedTime);
+  casesForICUByRequestedTime = Math.trunc((5/100) * infectionsByRequestedTime);
+  casesForVentilatorsByRequestedTime = Math.trunc((2/100) * infectionsByRequestedTime);
+  dollarsInFlight =Math.trunc((infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncome) / days);
+  dataOutPut.estmate.severeImpact = {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
   };
-  severeImpact();
   return dataOutPut;
 };
 
