54cac277852557656ea305d3623d51b9c1446862
kisekkamathias <kisekkamathias12@gmail.com>
2021-02-23 13:57:07 +0300

    fixing some errors

--- src/estimator.js
+++ src/estimator.js
@@ -12,14 +12,14 @@ const covid19ImpactEstimator = () => {
     region: {
       name: 'Africa',
       avgAge: 19.7,
-      avgDailyIncomeInUSD: 4,
-      avgDailyIncomePopulation: 0.73
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
     },
     periodType: 'days',
-    timeToElapse: 38,
-    reportedCases: 2747,
-    population: 92931687,
-    totalHospitalBeds: 678874
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
   };
   // Creating access to defferent objects of the inputData function
   const {
